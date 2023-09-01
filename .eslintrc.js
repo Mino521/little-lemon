f96c53281478e5dd9c7a069397c68209ab620448
Zeming Wang <wzm970527@gmail.com>
2023-09-01 16:50:49 +1000

    add prettier and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    // tsconfigRootDir: "",
+    project: ["./tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  root: true,
+  rules: {
+    "prettier/prettier": "error",
+    "no-constant-binary-expression": "error",
+    "no-duplicate-imports": "error",
+    "no-promise-executor-return": "error",
+    "no-self-compare": "error",
+    "arrow-body-style": ["error", "as-needed"],
+    "block-scoped-var": "error",
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      typescript project
-    </div>
-  );
+  return <div className="App">typescript project</div>;
 }
 
 export default App;
